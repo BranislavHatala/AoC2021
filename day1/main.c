d8c063243607fa2c05a11ef80efba1f0dda4fa77
Branislav Hatala <hatala.branislav@gmail.com>
2022-02-27 16:35:07 +0100

    day 1 fully functional

--- day1/main.c
+++ day1/main.c
@@ -1,7 +1,7 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby,
 C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
 Code, Compile, Run and Debug online from anywhere in world.
 
@@ -9,6 +9,7 @@ Code, Compile, Run and Debug online from anywhere in world.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 #define MAX 8
 
@@ -19,28 +20,29 @@ int main()
     int num;
     fp = fopen("input.txt", "r");
     int count = 0; //count of increases
-    int prev = 0; //previous
-    
-    if(fgets(buff, MAX, (FILE*)fp)) //get first value
-    {
-        num = atoi(buff);
-        prev = num;
-    }
-    else
-    {
-        printf("ERROR: reading from file failed.");
-    }
-    
+    int prev = INT_MAX; //previous
+
+    //get first delayed values, z delayed by time 1 and 2
+    fgets(buff, MAX, (FILE*)fp);
+    int z2 = atoi(buff);
+    fgets(buff, MAX, (FILE*)fp);
+    int z1 = atoi(buff);
+
     while(fgets(buff, MAX, (FILE*)fp)) //read and count value increases
     {
+        int z0 = atoi(buff);
+        int sum = (z0 + z1 + z2); // count sum
         num = atoi(buff);
         //printf("val: %d \n", number);
-        if(num > prev)
+        if(sum > prev)
         {
             count++;
         }
-        
-        prev = num;
+
+        prev = sum;
+        z2 = z1;
+        z1 = z0;
+
     }
     printf("Count of increases: %d \n", count);
     return 0;
